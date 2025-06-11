f3416ed6bb9768a83d128502ad4e0f90a7c09d77
Abhishek <abhishekgoel@gofynd.com>
2025-06-11 23:19:35 +0530

    hw

--- interviewQuestions.js
+++ interviewQuestions.js
@@ -65,3 +65,15 @@ function sayHi(age) {
 
 console.log(sayHi.call(person, 21));
 console.log(sayHi.bind(person, 21));
+
+//6th question 
+
+const firstPromise = new Promise((res, rej) => {
+    setTimeout(res, 500, 'one');
+  });
+  
+  const secondPromise = new Promise((res, rej) => {
+    setTimeout(res, 100, 'two');
+  });
+  
+  Promise.race([firstPromise, secondPromise]).then(res => console.log(res));
